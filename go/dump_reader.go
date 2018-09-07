5ab4b69e8f44a20e11915d93563955d7378b0337
Yuichiro Sawai <sawai.yuichiro.sn0@is.naist.jp>
2018-09-07 14:46:48 +0900

    Fix indentation

--- go/dump_reader.go
+++ go/dump_reader.go
@@ -1,85 +1,85 @@
 package main
 
 import (
-    "strings"
-    "os"
-    "log"
-    "encoding/xml"
+	"encoding/xml"
+	"log"
+	"os"
+	"strings"
 )
 
 type LangEntry struct {
-    title      string
-    oldid      string
-    sectionNum int
+	title      string
+	oldid      string
+	sectionNum int
 }
 
 func readDump(dumpPath string, lang Lang) <-chan LangEntry {
-    dumpFile, err := os.Open(dumpPath)
-    if err != nil {
-        log.Fatal(err)
-        return nil
-    }
-    entries := make(chan LangEntry)
+	dumpFile, err := os.Open(dumpPath)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	entries := make(chan LangEntry)
 
-    go func() {
-        decoder := xml.NewDecoder(dumpFile)
-        var currentId string
-        var currentTitle string
-        var currentText string
-        var tags []string
-        for {
-            t, err := decoder.Token()
-            if t == nil {
-                break
-            } else if err != nil {
-                log.Fatal(err)
-                break
-            }
-            switch e := t.(type) {
-            case xml.StartElement:
-                tags = append(tags, e.Name.Local)
-            case xml.CharData:
-                if len(tags) >= 1 {
-                    lastTag := tags[len(tags)-1]
-                    if lastTag == "title" {
-                        currentTitle = string(e)
-                    } else if len(tags) >= 2 && lastTag == "id" && tags[len(tags)-2] == "revision" {
-                        currentId = string(e)
-                    } else if lastTag == "text" {
-                        currentText = string(e)
-                    }
-                }
-            case xml.EndElement:
-                tags = tags[:len(tags)-1]
-                if e.Name.Local == "revision" {
-                    sectionNum := getSectionNumber(currentText, lang)
-                    if sectionNum > 0 {
-                        entries <- LangEntry{title: currentTitle, oldid: currentId, sectionNum: sectionNum}
-                    }
-                }
-            }
-        }
-        log.Println("Done reading dump.")
-        close(entries)
-        dumpFile.Close()
-    }()
+	go func() {
+		decoder := xml.NewDecoder(dumpFile)
+		var currentId string
+		var currentTitle string
+		var currentText string
+		var tags []string
+		for {
+			t, err := decoder.Token()
+			if t == nil {
+				break
+			} else if err != nil {
+				log.Fatal(err)
+				break
+			}
+			switch e := t.(type) {
+			case xml.StartElement:
+				tags = append(tags, e.Name.Local)
+			case xml.CharData:
+				if len(tags) >= 1 {
+					lastTag := tags[len(tags)-1]
+					if lastTag == "title" {
+						currentTitle = string(e)
+					} else if len(tags) >= 2 && lastTag == "id" && tags[len(tags)-2] == "revision" {
+						currentId = string(e)
+					} else if lastTag == "text" {
+						currentText = string(e)
+					}
+				}
+			case xml.EndElement:
+				tags = tags[:len(tags)-1]
+				if e.Name.Local == "revision" {
+					sectionNum := getSectionNumber(currentText, lang)
+					if sectionNum > 0 {
+						entries <- LangEntry{title: currentTitle, oldid: currentId, sectionNum: sectionNum}
+					}
+				}
+			}
+		}
+		log.Println("Done reading dump.")
+		close(entries)
+		dumpFile.Close()
+	}()
 
-    return entries
+	return entries
 }
 
 func getSectionNumber(text string, lang Lang) int {
-    langTitle := langTitles[lang]
-    sectionNum := 0
-    for _, line := range strings.Split(text, "\n") {
-        if strings.HasPrefix(line, "==") {
-            sectionNum++
-            for _, t := range langTitle {
-                // remove whitespaces
-                if strings.Replace(line, " ", "", -1) == strings.Replace(t, " ", "", -1) {
-                    return sectionNum
-                }
-            }
-        }
-    }
-    return 0
+	langTitle := langTitles[lang]
+	sectionNum := 0
+	for _, line := range strings.Split(text, "\n") {
+		if strings.HasPrefix(line, "==") {
+			sectionNum++
+			for _, t := range langTitle {
+				// remove whitespaces
+				if strings.Replace(line, " ", "", -1) == strings.Replace(t, " ", "", -1) {
+					return sectionNum
+				}
+			}
+		}
+	}
+	return 0
 }
--- go/languages.go
+++ go/languages.go
@@ -3,39 +3,39 @@ package main
 type Lang int
 
 const (
-    UnknownLanguage Lang = iota
-    EnEs
-    EnEn
-    EnDe
-    EnZh
-    EnKo
-    JaKo
+	UnknownLanguage Lang = iota
+	EnEs
+	EnEn
+	EnDe
+	EnZh
+	EnKo
+	JaKo
 )
 
 func getLang(langStr string) Lang {
-    switch langStr {
-    case "en-es":
-        return EnEs
-    case "en-en":
-        return EnEn
-    case "en-de":
-        return EnDe
-    case "en-zh":
-        return EnZh
-    case "en-ko":
-        return EnKo
-    case "ja-ko":
-        return JaKo
-    default:
-        return UnknownLanguage
-    }
+	switch langStr {
+	case "en-es":
+		return EnEs
+	case "en-en":
+		return EnEn
+	case "en-de":
+		return EnDe
+	case "en-zh":
+		return EnZh
+	case "en-ko":
+		return EnKo
+	case "ja-ko":
+		return JaKo
+	default:
+		return UnknownLanguage
+	}
 }
 
 var langTitles = map[Lang][]string{
-    EnEs: {"==Spanish=="},
-    EnEn: {"==English=="},
-    EnDe: {"==German=="},
-    EnKo: {"==Korean=="},
-    EnZh: {"==Chinese=="},
-    JaKo: {"==朝鮮語==", "=={{ko}}==", "=={{kor}}==", "=={{kor-KP}}==", "=={{kor-KR}=="},
+	EnEs: {"==Spanish=="},
+	EnEn: {"==English=="},
+	EnDe: {"==German=="},
+	EnKo: {"==Korean=="},
+	EnZh: {"==Chinese=="},
+	JaKo: {"==朝鮮語==", "=={{ko}}==", "=={{kor}}==", "=={{kor-KP}}==", "=={{kor-KR}=="},
 }
